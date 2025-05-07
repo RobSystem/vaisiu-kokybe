609d10c42acd4aafc369f4f5b240abb715b788e7
RobSystem <info@rochecks.nl>
2025-05-08 01:21:30 +0200

    EXTRA

--- src/pages/CreateSample.jsx
+++ src/pages/CreateSample.jsx
@@ -160,7 +160,8 @@ const [diameterExtra, setDiameterExtra] = useState([]);
           />
         </div>
       ))}
-    </div> className="flex justify-end mt-4"
+    </div>
+    <div className="flex justify-end mt-4">
   <button
     onClick={handleSave}
     className="bg-green-500 hover:bg-green-600 text-white text-sm px-4 py-2 rounded"
@@ -169,7 +170,7 @@ const [diameterExtra, setDiameterExtra] = useState([]);
     Save
   </button>
 </div>
-
+  </div>
 )}
 </div>
 

bcf67147d0a2b4b595af8440438286abb6595137
RobSystem <info@rochecks.nl>
2025-05-07 20:56:52 +0200

    Allreports 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        'xxs': '10px',
+        'xxxs': '9px',
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}

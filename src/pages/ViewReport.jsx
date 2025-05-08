5d2bf5a2d0628e11b2a2f09c6b3849f013fdc218
RobSystem <info@rochecks.nl>
2025-05-08 08:40:02 +0200

    Color

--- src/pages/ViewReport.jsx
+++ src/pages/ViewReport.jsx
@@ -149,7 +149,7 @@ function ViewReport() {
             </div>
           </div>
 
-          <div className="flex gap-6 mt-4">
+          <div className="flex gap-6 mt-4 justify-end">
             <p className={"font-bold " + getColor(sample.quality_score, 'quality')}>Quality Score: {sample.quality_score}</p>
             <p className={"font-bold " + getColor(sample.storage_score, 'storage')}>Storage Score: {sample.storage_score}</p>
           </div>

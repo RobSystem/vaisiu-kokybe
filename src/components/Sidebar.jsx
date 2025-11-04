2e58f2de8c7dfb9ff73d48f612c488f8cc00e6e5
Robertas <info@rochecks.nl>
2025-11-04 20:54:10 +0100

    Dashboard update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { supabase } from '../supabaseClient';
+ import { supabase } from "../supabaseClient";
 
 function Sidebar({ navigate, onLogout }) {
   const [role, setRole] = useState(null);
